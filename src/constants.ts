cbfbcebcb089e3129681d29f06826561352c299f
yayadontknow <110214688+yayadontknow@users.noreply.github.com>
2025-11-26 21:58:18 +0800

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 import { SpeciesId } from "#enums/species-id";
 
 /** The maximum size of the player's party */
-export const PLAYER_PARTY_MAX_SIZE: number = 6;
+export const PLAYER_PARTY_MAX_SIZE: number = 65;
 
 /** Whether to use seasonal splash messages in general */
 export const USE_SEASONAL_SPLASH_MESSAGES: boolean = true;
